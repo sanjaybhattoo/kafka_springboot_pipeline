c16dbfd8bc2cbc0cc00c62e8e8199e0a289e1948
sanjaybhattoo <56192215+sanjaybhattoo@users.noreply.github.com>
2024-09-23 18:20:04 -0700

    All Data available

---
+++ springboot-kafkaproject/producerkafka/src/main/java/net/java/springboot/springbootapplicationproducer.java
@@ -0,0 +1,20 @@
+package net.java.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class springbootapplicationproducer implements CommandLineRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(springbootapplicationproducer.class);
+    }
+    @Autowired
+    private WikimediaChangesProducer wikimediaChangesProducer;
+
+    @Override
+    public void run(String... args) throws Exception {
+        wikimediaChangesProducer.sendMessage();
+    }
+}
---
+++ springboot-kafkaproject/src/main/java/net/java/springboot/SpringbootKafkaprojectApplication.java
@@ -0,0 +1,13 @@
+package net.java.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaprojectApplication.class, args);
+	}
+
+}
